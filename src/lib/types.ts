271138dd7eeebcf3544a7cbe3e1c0c6f6147a001
Ben Hanzl <ben@benhanzl.com>
2020-02-08 13:33:10 -0600

    Define database document structures

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,14 +2,56 @@ import { Collection, ObjectId } from "mongodb";
 
 export interface Booking {
   _id: ObjectId;
+  listing: ObjectId;
+  tenant: string;
+  checkIn: string;
+  checkOut: string;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear;
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingsIndexMonth;
+}
+
+export interface BookingsIndexMonth {
+  [key: string]: boolean;
 }
 
 export interface Listing {
   _id: ObjectId;
+  address: string;
+  admin: string;
+  bookings: ObjectId[];
+  bookingsIndex: BookingsIndex;
+  city: string;
+  country: string;
+  description: string;
+  host: string;
+  image: string;
+  numOfGuests: number;
+  price: number;
+  title: string;
+  type: ListingType;
+}
+
+export enum ListingType {
+  Apartment = "APARTMENT",
+  House = "HOUSE"
 }
 
 export interface User {
-  _id: ObjectId;
+  _id: string;
+  avatar: string;
+  bookings: ObjectId[];
+  contact: string;
+  income: number;
+  listings: ObjectId[];
+  name: string;
+  token: string;
+  walletId?: string;
 }
 
 export interface Database {
